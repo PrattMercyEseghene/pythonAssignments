05071d3ee7dd8806b53ed47c5cb46f0359d883d3
PrattMercy <prattmercy3@gmail.com>
2026-05-10 17:55:02 +0100

    multiplicatin_table

--- CornFlakes/multiplicatin_table.py
+++ CornFlakes/multiplicatin_table.py
@@ -1,5 +1,17 @@
-for i in range (1, 11):
-    for j in range(1,11):
+#for i in range (1, 11):
+#    for j in range(1,11):
+#
+#     print(f"{i*j:4}",end=" ")
+#print()
 
-     print(f"{i*j:4}",end=" ")
-print()
+#number = 3
+#for i in range(1,13):
+#    print(number, " * ", i, " = ", number*i)
+#    
+
+for number in range(1, 13):
+
+    print(number)
+
+    for i in range(1, 13):
+        print(f"{number} * {i} = {number * i}")
